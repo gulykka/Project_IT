6a41a5e390a40dce941885cead9ea7ced7c4ba8a
gulykka <103120773+gulykka@users.noreply.github.com>
2023-01-23 06:11:36 +1000

    add repositories

--- Domain/Models/Profile.cs
+++ Domain/Models/Profile.cs
@@ -4,5 +4,11 @@ public class Profile
 {
     public int Id { get; set; }
     public string Name { get; set; }
+
+    public Profile(int id, String name)
+    {
+        Id = id;
+        Name = name;
+    }
     
 }
