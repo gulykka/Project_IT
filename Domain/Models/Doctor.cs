604b2c35eb72008d174399535e35609cd60fc5de
gulykka <103120773+gulykka@users.noreply.github.com>
2023-01-23 04:53:34 +1000

    add Doctor konstruktor

--- Domain/Models/Doctor.cs
+++ Domain/Models/Doctor.cs
@@ -2,8 +2,15 @@
 
 public class Doctor
 {
-    public int ID { get; set; }
+    public int Id { get; set; }
     public string FullName { get; set; }
     public Profile Profile { get; set; }
-    
+
+
+    public Doctor(int id, string fullName, Profile profile)
+    {
+        Id = id;
+        FullName = fullName;
+        Profile = profile;
+    }
 }
