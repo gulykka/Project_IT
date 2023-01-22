b198e7917600d0ffeb3779165b3fd5267a47ac9d
gulykka <103120773+gulykka@users.noreply.github.com>
2023-01-23 05:24:03 +1000

    add Reception konstruktor

--- Domain/Models/Reception.cs
+++ Domain/Models/Reception.cs
@@ -2,8 +2,19 @@
 
 public class Reception
 {
+    public int Id;
     public DateTime StartTime { get; set; }
     public DateTime EndTime { get; set; }
     public int UserId { get; set; }
     public int DoctorId { get; set; }
+
+
+    public Reception(int id, DateTime startTime, DateTime endTime, int userId, int doctorId)
+    {
+        Id = id;
+        StartTime = startTime;
+        EndTime = endTime;
+        UserId = userId;
+        DoctorId = doctorId;
+    }
 }
